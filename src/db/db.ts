9cc9fac506981a6100c17a0c8498f7d690b68d35
Hazel Phaho <Hazelphaho@gmail.com>
2024-10-12 07:13:57 +0200

    fix db connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,11 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
+config();
 
-config({ path: ".env" });
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set.");
+}
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle(sql);
